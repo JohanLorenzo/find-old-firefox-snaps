f7eb7921e5dd0afca287fc317df53d29e509084a
Johan Lorenzo <jlorenzo@mozilla.com>
2019-02-20 15:08:28 +0100

    Script works by extracting data from ship-it v2

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ import json
 from os import listdir
 from os.path import isfile, join
 
+import requests
+
 from taskcluster import Queue
 from taskcluster.exceptions import TaskclusterRestFailure
 from taskhuddler import TaskGraph
@@ -133,41 +135,72 @@ KNOWN_VERSIONS_AND_BUILDS = (
 )
 
 
-def get_json_files_paths():
-    json_folder = '/home/jlorenzo/git/mozilla-releng/releasewarrior-data/archive/firefox'
-    only_files = (
-        join(json_folder, f) for f in listdir(json_folder) if isfile(join(json_folder, f))
+# def get_json_files_paths():
+#     json_folder = '/home/jlorenzo/git/mozilla-releng/releasewarrior-data/archive/firefox'
+#     only_files = (
+#         join(json_folder, f) for f in listdir(json_folder) if isfile(join(json_folder, f))
+#     )
+#     only_json = (f for f in only_files if f.endswith('.json'))
+#     only_old_enough_version = (f for f in only_json if '-59' in f)
+#     return only_old_enough_version
+
+
+# def get_promote_graph_ids(json_path):
+#     with open(json_path) as f:
+#         json_data = json.load(f)
+#
+#     version = json_data['version']
+#
+#     for release in json_data['inflight']:
+#         build_number = release['buildnum']
+#         graphids = release['graphids']
+#         if len(graphids) > 0:
+#
+#             # import pdb; pdb.set_trace()
+#
+#             if isinstance(graphids[0], list):
+#                 promote_graph_ids = [
+#                     promote_graph_id
+#                     for graph_type, promote_graph_id in graphids
+#                     if graph_type in ('promote', 'promote_rc')
+#                 ]
+#             else:
+#                 promote_graph_ids = [graphids[0]]
+#             if len(promote_graph_ids) != 1:
+#                 raise Exception('version "{}" build {} does not have a singular promote graph: {}'.format(version, build_number, graphids))
+#
+#             yield promote_graph_ids[0], version, build_number
+
+
+def get_promote_graph_ids():
+    all_releases = requests.get('https://shipit-api.mozilla-releng.net/releases?status=shipped').json()
+    firefox_only = (
+        release
+        for release in all_releases
+        if release.get('product') == 'firefox'
     )
-    only_json = (f for f in only_files if f.endswith('.json'))
-    only_old_enough_version = (f for f in only_json if '-59' in f)
-    return only_old_enough_version
+    with_phases = (
+        release
+        for release in firefox_only
+        if release.get('phases')
+    )
+
+    for release in with_phases:
+        version = release['version']
+        build_number = release['build_number']
+
+        promote_graph_ids = [
+            phase['actionTaskId']
+            for phase in release['phases']
+            if phase['name'] in ('promote_firefox', 'promote_firefox_rc')
+        ]
+
+        if len(promote_graph_ids) != 1:
+            raise Exception('version "{}" build {} does not have a singular promote graph: {}'.format(version, build_number, release))
+
+        yield promote_graph_ids[0], version, build_number
 
 
-def get_promote_graph_ids(json_path):
-    with open(json_path) as f:
-        json_data = json.load(f)
-
-    version = json_data['version']
-
-    for release in json_data['inflight']:
-        build_number = release['buildnum']
-        graphids = release['graphids']
-        if len(graphids) > 0:
-
-            # import pdb; pdb.set_trace()
-
-            if isinstance(graphids[0], list):
-                promote_graph_ids = [
-                    promote_graph_id
-                    for graph_type, promote_graph_id in graphids
-                    if graph_type in ('promote', 'promote_rc')
-                ]
-            else:
-                promote_graph_ids = [graphids[0]]
-            if len(promote_graph_ids) != 1:
-                raise Exception('version "{}" build {} does not have a singular promote graph: {}'.format(version, build_number, graphids))
-
-            yield promote_graph_ids[0], version, build_number
 
 
 def get_snap_task_id(promote_graph_id):
@@ -201,33 +234,33 @@ def get_artifacts_urls(snap_task_id):
     for path in artifacts_paths:
         yield 'https://queue.taskcluster.net/v1/task/{}/artifacts/{}'.format(snap_task_id, path)
 
-for json_path in get_json_files_paths():
-    for promote_graph_id, version, build_number in get_promote_graph_ids(json_path):
-        if (version, build_number) in KNOWN_VERSIONS_AND_BUILDS:
-            print('Skipping {} build {}'.format(version, build_number))
+# for json_path in get_json_files_paths():
+for promote_graph_id, version, build_number in get_promote_graph_ids():
+    if (version, build_number) in KNOWN_VERSIONS_AND_BUILDS:
+        print('Skipping {} build {}'.format(version, build_number))
+        continue
+
+    try:
+        promote_task = queue.status(promote_graph_id)
+        if promote_task['status']['state'] != 'completed':
             continue
-
-        try:
-            promote_task = queue.status(promote_graph_id)
-            if promote_task['status']['state'] != 'completed':
-                continue
-        except TaskclusterRestFailure as e:
-            if e.status_code == 404:
-                print('Skipping {} build {} because promote task does not exist anymore'.format(version, build_number))
-                continue
-            else:
-                raise
-
-        snap_task_id, is_completed = get_snap_task_id(promote_graph_id)
-
-        if not is_completed:
+    except TaskclusterRestFailure as e:
+        if e.status_code == 404:
+            print('Skipping {} build {} because promote task does not exist anymore'.format(version, build_number))
             continue
+        else:
+            raise
 
-        for artifact_url in get_artifacts_urls(snap_task_id):
-            is_checksum = artifact_url.endswith('.checksums')
-            destination_url = 'https://archive.mozilla.org/pub/firefox/candidates/{version}-candidates/build{build_number}/snap/firefox-{version}.snap{postfix}'.format(
-                version=version[:-len('rc')] if version.endswith('rc') else version,
-                build_number=build_number,
-                postfix='.checksums' if is_checksum else ''
-            )
-            print(destination_url, artifact_url)
+    snap_task_id, is_completed = get_snap_task_id(promote_graph_id)
+
+    if not is_completed:
+        continue
+
+    for artifact_url in get_artifacts_urls(snap_task_id):
+        is_checksum = artifact_url.endswith('.checksums')
+        destination_url = 'https://archive.mozilla.org/pub/firefox/candidates/{version}-candidates/build{build_number}/snap/firefox-{version}.snap{postfix}'.format(
+            version=version[:-len('rc')] if version.endswith('rc') else version,
+            build_number=build_number,
+            postfix='.checksums' if is_checksum else ''
+        )
+        print(destination_url, artifact_url)
